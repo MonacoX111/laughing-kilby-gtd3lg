8e92d71254e3500d1fc9c725ca9cb2c9392b89af
maga <stasbandura200620062006@gmail.com>
2026-04-14 21:19:58 +0000

    change

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlBETYBCdsYkk0nluitALQH9Q0gGoU_bI",
-  authDomain: "sansara-data.firebaseapp.com",
-  projectId: "sansara-data",
-  storageBucket: "sansara-data.firebasestorage.app",
-  messagingSenderId: "951389320456",
-  appId: "1:951389320456:web:4d8f82641becc5f45d68bd",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
